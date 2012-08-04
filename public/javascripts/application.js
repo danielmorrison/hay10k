74ae7533ac67e548af506584daaa230cb2407513
Daniel Morrison <daniel@collectiveidea.com>
2012-08-04 10:57:23 -0400

    Update JS files

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,2 @@
-// (function($) {
-//   $.ajaxSettings.accepts.html = $.ajaxSettings.accepts.script;
-//   
-//   $.authenticityToken = function() {
-//     return $('#authenticity-token').attr('content');
-//   };
-//   
-//   $(document).ajaxSend(function(event, request, settings) {
-//     if (settings.type == 'post') {
-//       settings.data = (settings.data ? settings.data + "&" : "")
-//           + "authenticity_token=" + encodeURIComponent($.authenticityToken());
-//       request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//     }
-//   });
-// })(jQuery);
-// 
-// 
-// (function($) {
-//   $('form').live('change', function(e) {
-//     alert('here')
-//   });
-// })(jQuery);
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
