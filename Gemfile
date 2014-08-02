a4400c4e2b1b04952d38ff10c337b919a5ccdac3
Daniel Morrison <daniel@collectiveidea.com>
2014-08-01 23:31:31 -0400

    upgrade rails and address an asset bug

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source "https://rubygems.org"
+
+ruby "2.1.2"
+
 gem 'rails', '~> 3.0.0'
 gem 'mysql2', "~> 0.2.7"
 # gem 'acts_as_geocodable', '1.0.4'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+  config.relative_url_root = "" # see https://github.com/rails/rails/issues/9619
+
   helper :all # include all helpers, all the time
-  
+
   before_filter :find_year
-  
-  
-private  
+
+
+private
   def find_year
     @year = Year.find_by_year!(params[:year_id])
   end
