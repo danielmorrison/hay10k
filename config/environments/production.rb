41793602dcb43ecb495db6472d352d98c806462e
Daniel Morrison <daniel@collectiveidea.com>
2019-08-01 15:46:12 -0400

    Upgrade puma Remove extraneous ssl lines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  require "rack/ssl"
-  config.middleware.use Rack::SSL
 end
